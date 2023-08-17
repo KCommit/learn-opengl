20ce99be02af2511cd91b8db72857754c68ec9c6
KCommit <103474404+KCommit@users.noreply.github.com>
2023-08-18 07:25:06 +0800

    draw a triangle

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define GL_SILENCE_DEPRECATION
+#include <epoxy/gl.h>
 #include <GLFW/glfw3.h>
 
 int main(void) {
@@ -22,6 +23,12 @@ int main(void) {
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+    glFlush();
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
